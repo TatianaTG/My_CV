1c66783e10bfb92860a3e6120b37283f4e5f8c4a
TatianaTG <tatiana.teisan@gmail.com>
2023-01-25 17:34:51 +0200

    changes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,26 @@
-// $ == document.querySelector()
-$(document).ready(() => {
-    $(".diploma__content-slider").slick({
-        slidesToShow: 3,
-    });
-});
 
+const letter = document.querySelector(".letter__wrapper")
+
+letter.addEventListener("mouseover", () => {
+    letter.classList.add("flap")
+})
+
+letter.addEventListener("mouseleave", () => {
+    letter.classList.remove("flap")
+})
+
+document.querySelector(".letter__text").addEventListener("click", () => {
+    document.querySelector(".letter__overlay").classList.add("active")
+})
+
+document.querySelector(".letter__popup-close").addEventListener("click", () => {
+    document.querySelector(".letter__overlay").classList.remove("active")
+})
+
+const updateClock = () => {
+    var date = new Date()
+    var time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()
+    document.querySelector(".footer__content-clock").innerHTML = time
+    setTimeout(updateClock, 1000)
+}
+updateClock()
